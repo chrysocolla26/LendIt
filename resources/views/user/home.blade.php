b7040e09868e5235984e032064f725228fe3674a
Ssfeaeae <richie_blue10@yahoo.com>
2018-10-29 10:05:58 +0700

    Major updates to home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -8,39 +8,41 @@
 
 @section('content')
 	<div class="container">
-		<div class="new-offer">
-			
+		<div class="side-nav">
+			<div class="left-username">
+				<img src="images/backpack.png">
+				Richie Muliawan
+			</div>
+			<div class="left-nav">
+				<div class="link-menu">asssssssssssssssssssssssssassssssssss</div>
+				<div class="link-menu">2</div>
+				<div class="link-menu">3</div>
+				<div class="link-menu">3</div>
+				<div class="link-menu">3</div>
+				<div class="link-menu">3</div>
+				<div class="link-menu">3</div>
+			</div>
 		</div>
-		<div class="highlighted-product">
-			<div id="product1" class="product">
-				<div class="product-img">
-					<img src="images/camera.png">
+		<div class="main-content">
+			<div class="product">
+				<div class="product-title">
+					Game Boy
 				</div>
-				<div class="product-desc">
-					
-				</div>
-			</div>
-			<div id="product1" class="product">
-				<div class="product-img">
-					<img src="images/backpack.png">
-				</div>
-				<div class="product-desc">
-					
-				</div>
-			</div>
-			<div id="product1" class="product">
 				<div class="product-img">
 					<img src="images/gameboy.png">
 				</div>
 				<div class="product-desc">
-					
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
 				</div>
 			</div>
-{{-- 			<div id="product2" class="product"></div>
-			<div id="product3" class="product"></div>
-			<div id="product4" class="product"></div>
-			<div id="product5" class="product"></div> --}}
 		</div>
+		<div class="side-news">3</div>
+		<div class="side-chat">4</div>
 	</div>
 @endsection
 
