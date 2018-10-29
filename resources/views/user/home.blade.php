42bfdac8f9afe4148f9b3ea678653af111886334
Ssfeaeae <richie_blue10@yahoo.com>
2018-10-29 11:17:26 +0700

    home update

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -14,22 +14,66 @@
 				Richie Muliawan
 			</div>
 			<div class="left-nav">
-				<div class="link-menu">asssssssssssssssssssssssssassssssssss</div>
-				<div class="link-menu">2</div>
-				<div class="link-menu">3</div>
-				<div class="link-menu">3</div>
-				<div class="link-menu">3</div>
-				<div class="link-menu">3</div>
-				<div class="link-menu">3</div>
+				<div class="link-menu">Home</div>
+				<div class="link-menu">Message</div>
+				<div class="link-menu">Marketplace</div>
 			</div>
 		</div>
 		<div class="main-content">
-			<div class="product">
+			<div id="product1" class="product">
 				<div class="product-title">
-					Game Boy
+					Richie Muliawan
+					<span class="post-time">2w ago</span>
 				</div>
 				<div class="product-img">
-					<img src="images/gameboy.png">
+					<table>
+						<tr>
+							<td><img src="images/nintendo-switch.jpg"></td>
+						</tr>
+					</table>
+					
+				</div>
+				<div class="product-desc">
+					<table>
+						<tr>
+							<td>Nama</td>
+							<td>:</td>
+							<td>Nintendo Switch (Black)</td>
+						</tr>
+						<tr>
+							<td>Stock</td>
+							<td>:</td>
+							<td>1</td>
+						</tr>
+						<tr>
+							<td>Deskripsi</td>
+							<td>:</td>
+							<td>Durasi pinjam minimal 1 minggu. Kondisi 98% bagus</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div id="product2" class="product">
+				<div class="product-title">
+					Camera
+				</div>
+				<div class="product-img">
+					<img src="images/camera.png">
+				</div>
+				<div class="product-desc">
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+					Field 1 : <br>
+				</div>
+			</div><div id="product1" class="product">
+				<div class="product-title">
+					Backpack
+				</div>
+				<div class="product-img">
+					<img src="images/backpack.png">
 				</div>
 				<div class="product-desc">
 					Field 1 : <br>
@@ -41,8 +85,8 @@
 				</div>
 			</div>
 		</div>
-		<div class="side-news">3</div>
-		<div class="side-chat">4</div>
+		<div class="side-news">News</div>
+		<div class="side-chat">Chat</div>
 	</div>
 @endsection
 
