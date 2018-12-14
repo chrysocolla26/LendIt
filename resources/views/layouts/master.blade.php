f3248d3d7551b337c036488d1f76c1cf2e49db96
FaceBlank <hansel.septiadi@gmail.com>
2018-12-14 13:31:22 +0700

    Iseng ubah nav bar atas

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,27 +22,26 @@
 
 	      	<div class="collapse navbar-collapse" id="navbarsExample03">
 	        	<ul class="navbar-nav mr-auto">
+	        		<form class="form-inline my-2 my-md-0">
+	          			<input class="form-control" type="text" placeholder="Search">
+        			</form>
 	          		<li class="nav-item active">
 	            		<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
 	          		</li>
 	          		<li class="nav-item">
-	            		<a class="nav-link" href="#">Link</a>
-	          		</li>
-	          		<li class="nav-item">
-	            		<a class="nav-link disabled" href="#">Disabled</a>
+	            		<a class="nav-link" href="#">Profile</a>
 	          		</li>
 	          		<li class="nav-item dropdown">
-	            		<a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+	            		<a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
 	            		<div class="dropdown-menu" aria-labelledby="dropdown03">
-	              			<a class="dropdown-item" href="#">Action</a>
-	              			<a class="dropdown-item" href="#">Another action</a>
-	              			<a class="dropdown-item" href="#">Something else here</a>
+	              			<a class="dropdown-item" href="#">Lend</a>
+	              			<a class="dropdown-item" href="#">Borrow</a>
 	           		 	</div>
 	          		</li>
-	        	</ul>
-	        	<form class="form-inline my-2 my-md-0">
-	          		<input class="form-control" type="text" placeholder="Search">
-        		</form>
+	          		<li class="nav-item">
+	            		<a class="nav-link" href="#">About Us</a>
+	          		</li>
+	        	</ul>	        	
 	      	</div>
 	    </nav>
         <div class="page-content">
