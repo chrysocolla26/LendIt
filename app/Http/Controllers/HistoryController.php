25a8979e42eebd1880016d2c359b4c44757d9752
chrysocolla26 <klemenslr12@gmail.com>
2019-01-12 00:12:36 +0700

    update lend-history

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -14,7 +14,17 @@ class HistoryController extends Controller
 {
     public function borrowHistory(){
         $borrows = Borrow::where('borrower_id', '=', Session("id"))->get();
-
     	return view('history.borrow-history',compact('borrows'));
+    }
+
+    public function lendHistory(){
+    	$posts = Post::where('user_id', '=', Session("id"))->get();
+
+    	foreach ($posts as $post) {
+    		$lends = Borrow::where('product_id', '=', $post->id)->get();
+    		if($lends)
+    			break;
+    	}
+    	return view('history.lend-history',compact('lends'));
     }
 }
